3884d4a1572cce5b527e49afe4accbaf113311a3
OMARcod <31794534+OMARcod@users.noreply.github.com>
2022-09-12 23:30:50 +0200

    Add New game
    added the tictactoe game
    with all the functions and variables

--- Uppgift 01 Kasino delmoment 1.1.cpp
+++ Uppgift 01 Kasino delmoment 1.1.cpp
@@ -18,14 +18,15 @@ int GuessGame(int& aWalletMoney);
 int GuessGameMenu(int& aWalletMoney);
 void MoneyMath(int& aWalletMoney, int someAmountOfMoney);
 
-
 //New Functions
-bool checkIfEmptyAt(int anIndex);
-void StoreWinGame(bool isWin);
+bool CheckIfEmptyAt(int anIndex);
+void StorWinGame(bool isWin);
 bool CheckIfReachMaxWinMoney(int aMaxWinMoney);
 void DisplayYouCantPlay();
-void subtractFromWinMoney(int aWinMoney, const int& amountToSubtract);
-void showAmountOfWin(const int& aWinMoney);
+void SubtractFromWinMoney(int aWinMoney, const int& amountToSubtract);
+void ShowAmountOfWin(const int& aWinMoney);
+void DisplayWinLose();
+
 
 static const int locMaxMoney = 5000;
 static int locWinMoneyGuess = 0;
@@ -35,7 +36,39 @@ static int locMaxWinMoneyGameThree = 0;
 static const int arraySize = 5;
 int historyWin[arraySize] = { 0 };
 
+//TicTacToe
+void ChangeArray(char a[][3], char letter);
+void ChangeArrayAtLocation(char a[][3], char letter, int index, int player);
+void DipslayArray(char a[][3]);
+int AskForInputGame(std::string playerName);
+int AskForInputMenu();
+int CheckInput();
+bool CheckIndexIsUsed(int index);
+bool CheckIfFull();
+bool CheckIfWin();
+bool CheckHorisontalWin();
+bool CheckVerticalWin();
+bool CheckCurvedLineWin();
+bool CheckThisRow(int beginIndex, int player, int toAdd);
+void ShowMenu();
+void ResetTheGame(char array[][3]);
+int RandomIndex();
+int EasyEnemi();
+int RandomNumber(int aMin, int aMax);
+int RandomIndex();
+int returnEmptyIndex();
+int PlayTicTacToe();
 
+static int const arraySizeSpace = 9;
+static int usedSpace[arraySizeSpace]{ 0 };
+
+enum class Player
+{
+	X = 1,
+	O = 2,
+};
+int playerO = static_cast<int>(Player::O);
+int playerX = static_cast<int>(Player::X);
 
 enum class Instruction
 {
@@ -45,12 +78,9 @@ enum class Instruction
 	Empty = 0,
 };
 
-
 int main()
 {
 	int walletMoney = 1000;
-
-
 
 	Play(walletMoney);
 
@@ -92,6 +122,11 @@ void Play(int& aWalletMoney)
 		case 3:
 			system("cls");
 			GamesRules();
+			inputNr = PlayTicTacToe();
+			break;
+		case 4:
+			system("cls");
+			GamesRules();
 			inputNr = CheckExit();
 			break;
 		case 0:
@@ -128,9 +163,12 @@ int ShowMenu(int aWalletMoney)
 	std::cout << std::endl;
 	std::cout << "1. Play: guess game" << std::endl;
 	std::cout << "2. Play: odd and even numbers" << std::endl;
-	std::cout << "3. Read the rules" << std::endl;
+	std::cout << "3. Play TicTacToe" << std::endl;
+	std::cout << "4. Read the rules" << std::endl;
 	std::cout << "0. Exit!" << std::endl;
 	ShowMoneyLeft(aWalletMoney);
+	DisplayWinLose();
+
 	int inputNr = ReadInputInteger();
 
 	return inputNr;
@@ -175,6 +213,9 @@ int PlayOddAndEven(int& aWalletMoney)
 	int inputNr = 0;
 	int result = 0;
 
+	bool win = true;
+	bool lose = false;
+
 	bool wrongGuess = true;
 	bool isPlaying = true;
 
@@ -217,7 +258,7 @@ int PlayOddAndEven(int& aWalletMoney)
 			amountOfMoney = AskAmountMoney(aWalletMoney);
 
 		}
-		showAmountOfWin(locWinMoneyOddEven);
+		ShowAmountOfWin(locWinMoneyOddEven);
 		std::cout << "I have thrown 2 dices!  ";
 		std::cout << "What do want to chose? odd or even" << std::endl;
 		std::cout << std::endl;
@@ -252,6 +293,7 @@ int PlayOddAndEven(int& aWalletMoney)
 
 					std::cout << "congratulatiions you guessed right" << std::endl;
 					wrongGuess = false;
+					StorWinGame(win);
 
 					std::cout << "Do you want to play one more time ? y/n" << std::endl;
 
@@ -287,6 +329,8 @@ int PlayOddAndEven(int& aWalletMoney)
 
 					std::cout << "congratulatiions you guessed right" << std::endl;
 					wrongGuess = false;
+					StorWinGame(win);
+
 
 
 					std::cout << "Do you want to play one more time ? y/n" << std::endl;
@@ -308,7 +352,7 @@ int PlayOddAndEven(int& aWalletMoney)
 				system("cls");
 
 				MoneyMath(aWalletMoney, -amountOfMoney);
-				subtractFromWinMoney(locWinMoneyOddEven, amountOfMoney);
+				SubtractFromWinMoney(locWinMoneyOddEven, amountOfMoney);
 
 
 				wrongGuess = false;
@@ -324,6 +368,8 @@ int PlayOddAndEven(int& aWalletMoney)
 
 					std::cout << "you have lost!" << std::endl;
 					std::cout << "Do you want to play one more time ? y/n" << std::endl;
+					StorWinGame(lose);
+
 
 					if (ReadInputChar() == 'n')
 					{
@@ -362,7 +408,7 @@ int OddAndEvenGameMenu(int& aWalletMoney)
 		std::cout << "1. Play" << std::endl;
 		std::cout << "2. Read the rules" << std::endl;
 		std::cout << "0. Exit!" << std::endl;
-		showAmountOfWin(locWinMoneyOddEven);
+		ShowAmountOfWin(locWinMoneyOddEven);
 
 
 		inputNr = ReadInputInteger();
@@ -509,14 +555,14 @@ void GamesRules()
 	std::cout << "0. Exit!" << std::endl;
 }
 
-void showAmountOfWin(const int& aWinMoney)
+void ShowAmountOfWin(const int& aWinMoney)
 {
 	std::cout << "=====================================" << std::endl;
 	std::cout << "amount of win " << aWinMoney << "$" << std::endl;
 	std::cout << "=====================================" << std::endl;
 }
 
-void subtractFromWinMoney(int aWinMoney, const int& amountToSubtract)
+void SubtractFromWinMoney(int aWinMoney, const int& amountToSubtract)
 {
 	if (aWinMoney > amountToSubtract)
 	{
@@ -550,6 +596,8 @@ int GuessGame(int& aWalletMoney)
 	int nrOfTry = nrOfTryAllowed;
 	bool wrongGuess = true;
 	bool isPlaying = true;
+	bool win = true;
+	bool lose = false;
 
 	int amountOfMoney = 0;
 	//int amountOfWinMoney = 0;
@@ -572,7 +620,7 @@ int GuessGame(int& aWalletMoney)
 		{
 			amountOfMoney = AskAmountMoney(aWalletMoney);
 		}
-		showAmountOfWin(locWinMoneyGuess);
+		ShowAmountOfWin(locWinMoneyGuess);
 		std::cout << "guess the sum of the two dices ? \t\t" << sumOfDices << std::endl;
 
 		std::cout << "Your Input: ";
@@ -601,7 +649,7 @@ int GuessGame(int& aWalletMoney)
 
 					MoneyMath(aWalletMoney, -amountOfMoney);
 
-					subtractFromWinMoney(locWinMoneyGuess, amountOfMoney);
+					SubtractFromWinMoney(locWinMoneyGuess, amountOfMoney);
 
 					wrongGuess = false;
 
@@ -613,6 +661,7 @@ int GuessGame(int& aWalletMoney)
 					else
 					{
 						std::cout << "you have lost!" << std::endl;
+						StorWinGame(lose);
 
 						if (CheckIfReachMaxWinMoney(locWinMoneyGuess))
 						{
@@ -668,6 +717,7 @@ int GuessGame(int& aWalletMoney)
 					std::cout << "==================" << std::endl;
 					std::cout << "congratulatiions you guessed right" << std::endl;
 					wrongGuess = false;
+					StorWinGame(win);
 
 					MoneyMath(aWalletMoney, amountOfMoney * 2);
 					locWinMoneyGuess += amountOfMoney * 2;
@@ -715,7 +765,7 @@ int GuessGameMenu(int& aWalletMoney)
 		std::cout << "1. Play" << std::endl;
 		std::cout << "2. Read the rules" << std::endl;
 		std::cout << "0. Exit!" << std::endl;
-		showAmountOfWin(locWinMoneyGuess);
+		ShowAmountOfWin(locWinMoneyGuess);
 
 		inputNr = ReadInputInteger();
 
@@ -770,20 +820,15 @@ int GuessGameMenu(int& aWalletMoney)
 	return static_cast<int>(Instruction::Exit);
 }
 
-////fucntion to Play the other casino game
-////funciton to display 5 last played games 
-////function to display the history of played games
-//
-////function to display if player won more than 400$ or if lose more than 400
 
-//fucntion to see if the player have won more than 5000 in a single game
+//New
 bool CheckIfReachMaxWinMoney(int aMaxWinMoney)
 {
 	bool isReached = aMaxWinMoney >= locMaxMoney;
 	return isReached;
 }
 
-void StoreWinGame(bool isWin)
+void StorWinGame(bool isWin)
 {
 	bool ok = true;
 	while (ok)
@@ -792,7 +837,7 @@ void StoreWinGame(bool isWin)
 		{
 			for (int i = 0; i < 5; i++)
 			{
-				if (checkIfEmptyAt(i))
+				if (CheckIfEmptyAt(historyWin[i]))
 				{
 					historyWin[i] = static_cast<int>(Instruction::Win);
 					return;
@@ -803,7 +848,7 @@ void StoreWinGame(bool isWin)
 		{
 			for (int i = 0; i < 5; i++)
 			{
-				if (checkIfEmptyAt(i))
+				if (CheckIfEmptyAt(historyWin[i]))
 				{
 					historyWin[i] = static_cast<int>(Instruction::Lose);
 					return;
@@ -814,21 +859,564 @@ void StoreWinGame(bool isWin)
 		//if is full
 		//stor the make the fist element emepty
 		//push the rest to one element to the right
+		
 		int temp = 0;
-		for (int i = 0; i < arraySize - 1; i++)
+		for (int i = 4; i > 0; i--)
 		{
-			temp = historyWin[i + 1];
-			historyWin[i + 1] = historyWin[i];
+			temp = historyWin[i - 1];
+			historyWin[i - 1] = historyWin[i];
+			historyWin[i] = temp;
 		}
 		historyWin[0] = static_cast<int>(Instruction::Empty);
 	}
+}
 
+bool CheckIfEmptyAt(int anIndex)
+{
+	return anIndex == static_cast<int>(Instruction::Empty);
+}
+
+void DisplayWinLose()
+{
+	for (int i = 0; i < 5; i++)
+	{
+		if (historyWin[i] == 1)
+		{
+			std::cout << i + 1 << ": win ";
+		}
+		else if (historyWin[i] == 2)
+		{
+			std::cout << i + 1 << ": lose ";
+		}
+		else
+		{
+			std::cout << i + 1 << ": empty ";
+		}
+		std::cout << "|| ";
+
+	}
+	std::cout << std::endl;
+}
+
+
+//TicTacToe
+
+int PlayTicTacToe()
+{
+	const int row = 3, column = 3;
+	char array2D[3][3];
+	int index = 0;
+
+
+	bool isPlaying = true;
+	bool firstPlayer = true;
+	ShowMenu();
+	index = AskForInputMenu();
+
+	std::string player1 = "";
+	std::string player2 = "";
+
+	bool GameNotExit = true;
+
+	if (index == 0)
+	{
+		GameNotExit = false;
+	}
+	else
+	{
+		system("cls");
+		std::cout << "First Player Name: ";
+		std::cin >> player1;
+
+
+		system("cls");
+		ChangeArray(array2D, ' ');
+		DipslayArray(array2D);
+		std::cout << "Type the the number of the box that you want to use:" << std::endl;
+	}
+	bool wantToPlay = true;
+
+	while (GameNotExit)
+	{
+		while (wantToPlay)
+		{
+			while (isPlaying)
+			{
+				if (firstPlayer)
+				{
+					index = AskForInputGame(player1);
+				}
+				else
+				{
+					index = returnEmptyIndex();
+				}
+				if (CheckIndexIsUsed(index))
+				{
+					std::cout << index << ": box is used! Pleas chose another box" << std::endl;
+				}
+				else
+				{
+					if (firstPlayer)
+					{
+						ChangeArrayAtLocation(array2D, 'X', index, static_cast<int>(Player::X));
+						firstPlayer = false;
+					}
+					else
+					{
+						ChangeArrayAtLocation(array2D, 'O', index, static_cast<int>(Player::O));
+						firstPlayer = true;
+					}
+					DipslayArray(array2D);
+				}
+				if (CheckIfWin())
+				{
+					if (firstPlayer)
+					{
+						std::cout << "the comp have Won!" << std::endl;
+						isPlaying = false;
+					}
+					else
+					{
+						std::cout << player2 << " You Won!" << std::endl;
+						isPlaying = false;
+					}
+				}
+				else if (CheckIfFull())
+				{
+					std::cout << "The box is full" << std::endl;
+					isPlaying = false;
+				}
+			}
+
+			std::cout << "==========================" << std::endl;
+			std::cout << "Do you want to play again?" << std::endl;
+			std::cout << "1. Yes" << std::endl;
+			std::cout << "0. No!" << std::endl;
+			if (AskForInputMenu() == 0)
+			{
+				wantToPlay = false;
+				ResetTheGame(array2D);
+			}
+			else
+			{
+				isPlaying = true;
+				ResetTheGame(array2D);
+			}
+		}
+
+		ShowMenu();
+		index = AskForInputMenu();
+		if (index == 0)
+		{
+			GameNotExit = false;
+		}
+		else
+		{
+			wantToPlay = true;
+			isPlaying = true;
+			system("cls");
+			std::cout << "First Player Name: ";
+			std::cin >> player1;
+			std::cout << "Secund Player Name: ";
+			std::cin >> player2;
+
+			system("cls");
+			ChangeArray(array2D, ' ');
+			DipslayArray(array2D);
+			std::cout << "Type the the number of the box that you want to use:" << std::endl;
+		}
+	}
+
+
+	return static_cast<int>(Instruction::Exit);
+}
+
+void ResetTheGame(char array[][3])
+{
+	for (int i = 0; i < arraySizeSpace; i++)
+	{
+		usedSpace[i] = 0;
+	}
+	ChangeArray(array, ' ');
+	DipslayArray(array);
+}
+
+int AskForInputGame(std::string playerName)
+{
+	int index = 0;
+	std::cout << playerName << " Input: ";
+
+	index = CheckInput();
+	while (index < 1 || index > arraySizeSpace)
+	{
+		std::cout << "Pleas Enter a number between 1 and 9 ...." << std::endl;
+		std::cout << "Your Input: ";
+		index = CheckInput();
+	}
+	return  index;
+}
+
+int EasyEnemi()
+{
+	int index = 0;
+
+	index = RandomIndex();
+	while (index < 1 || index > arraySizeSpace)
+	{
+		std::cout << "Pleas Enter a number between 1 and 9 ...." << std::endl;
+		std::cout << "Your Input: ";
+		index = RandomIndex();
+	}
+	return  index;
+}
+
+int RandomIndex()
+{
+
+	int lastIndex = arraySizeSpace - 1;
+	int index = RandomNumber(1, lastIndex);
+	bool isUsed = true;
+	while (isUsed)
+	{
+		if (usedSpace[index] > 0 && usedSpace[index] < 3)
+		{
+			index = RandomNumber(1, lastIndex);
+		}
+		else
+		{
+			return index;
+		}
+	}
+}
+
+int RandomNumber(int aMin, int aMax)
+{
+
+	std::uniform_int_distribution<int> rndDist(aMin, aMax);
+	int randomNr = rndDist(rndEngine);
+	return randomNr;
+}
+
+int returnEmptyIndex()
+{
+	int RandomLoop = RandomNumber(0, 10);
+
+
+	int random = 5;
+
+	for (int i = 0; i < 5; i++)
+	{
+		if (!(CheckIndexIsUsed(random)))
+		{
+			return random;
+		}
+		random = RandomNumber(1, 9);
+	}
+
+	if (RandomLoop > 5)
+	{
+		for (int i = 1; i < arraySizeSpace + 1; i++)
+		{
+			if (!(CheckIndexIsUsed(i)))
+			{
+				return i;
+			}
+		}
+	}
+	else
+	{
+		for (int i = arraySizeSpace; i > 0; i--)
+		{
+			if (!(CheckIndexIsUsed(i)))
+			{
+				return i;
+			}
+		}
+	}
 
 }
 
-bool checkIfEmptyAt(int anIndex)
+int AskForInputMenu()
 {
-	return anIndex == static_cast<int>(Instruction::Empty);
+	int index = 0;
+	std::cout << "Your Input: ";
+
+	index = CheckInput();
+	while (index < 0 || index > 1)
+	{
+		std::cout << "Pleas Enter 0 or 1 ...." << std::endl;
+		std::cout << "Your Input: ";
+		index = CheckInput();
+	}
+	return  index;
+}
+
+int CheckInput()
+{
+	int input;
+	std::cin >> input;
+	while (true)
+	{
+		if (std::cin.fail())
+		{
+			std::cin.clear();
+			std::cin.ignore(10000, '\n');
+			std::cout << "You have entered wrong input" << std::endl;
+			std::cin >> input;
+		}
+		if (!std::cin.fail())
+		{
+			std::cin.clear();
+			std::cin.ignore(10000, '\n');
+			break;
+		}
+	}
+	return input;
+}
+
+void DipslayArray(char a[][3])
+{
+	int boxNr = 1;
+
+	system("cls");
+	std::cout << "-----------------" << std::endl;
+	for (int i = 0; i < 3; i++)
+	{
+		for (int k = 0; k < 3; k++)
+		{
+			std::cout << " | " << a[i][k];
+		}
+		std::cout << " | "; //last 
+
+		//other box
+		std::cout << "\t\t";
+		for (int i = 0; i < 3; i++)
+		{
+			std::cout << " | " << boxNr;
+			boxNr++;
+		}
+		std::cout << " | "; //last 
+
+
+		std::cout << "\n-----------------" << std::endl;
+	}
+}
+
+void ChangeArray(char a[][3], char letter)
+{
+	for (int i = 0; i < 3; i++)
+	{
+		for (int k = 0; k < 3; k++)
+		{
+			a[i][k] = letter;
+		}
+	}
+}
+
+bool CheckIndexIsUsed(int index)
+{
+	bool isUsed = false;
+	if (usedSpace[index - 1] == 1 || usedSpace[index - 1] == 2)
+	{
+		isUsed = true;
+	}
+	return isUsed;
+}
+
+void ChangeArrayAtLocation(char a[][3], char letter, int index, int player)
+{
+	int row = 0;
+	int column = 0;
+
+	switch (index)
+	{
+	case 1:
+		row = 0;
+		column = 0;
+		usedSpace[index - 1] += player;
+
+		break;
+	case 2:
+		row = 0;
+		column = 1;
+		usedSpace[index - 1] += player;
+
+		break;
+	case 3:
+		row = 0;
+		column = 2;
+		usedSpace[index - 1] += player;
+		break;
+	case 4:
+		row = 1;
+		column = 0;
+		usedSpace[index - 1] += player;
+		break;
+	case 5:
+		row = 1;
+		column = 1;
+		usedSpace[index - 1] += player;
+		break;
+	case 6:
+		row = 1;
+		column = 2;
+		usedSpace[index - 1] += player;
+		break;
+	case 7:
+		row = 2;
+		column = 0;
+		usedSpace[index - 1] += player;
+		break;
+	case 8:
+		row = 2;
+		column = 1;
+		usedSpace[index - 1] += player;
+		break;
+	case 9:
+		row = 2;
+		column = 2;
+		usedSpace[index - 1] += player;
+		break;
+	default:
+		break;
+	}
+
+	a[row][column] = letter;
+}
+
+bool CheckIfFull()
+{
+	bool isFull = false;
+	int nrOfBoxesUsed = 0;
+	for (int i = 0; i < arraySizeSpace; i++)
+	{
+		if (usedSpace[i] == 1 || usedSpace[i] == 2)
+		{
+			nrOfBoxesUsed++;
+		}
+	}
+	if (nrOfBoxesUsed >= arraySizeSpace)
+	{
+		isFull = true;
+	}
+	return isFull;
+}
+
+bool CheckIfWin()
+{
+	bool isWin = CheckHorisontalWin();
+
+	if (!isWin)
+	{
+		isWin = CheckVerticalWin();
+
+		if (!isWin)
+		{
+			isWin = CheckCurvedLineWin();
+		}
+	}
+
+	return isWin;
+}
+
+bool CheckHorisontalWin()
+{
+	bool isWin = false;
+	int beginIndex = 0;
+	int row = 3;
+	for (int i = 0; i < row; i++)
+	{
+		if (CheckThisRow(beginIndex, playerX, 1))
+		{
+			isWin = true;
+			break;
+		}
+		else if (CheckThisRow(beginIndex, playerO, 1))
+		{
+			isWin = true;
+			break;
+		}
+		beginIndex += 3;
+	}
+
+	return isWin;
+}
+
+bool CheckVerticalWin()
+{
+	bool isWin = false;
+	int beginIndex = 0;
+	int column = 3;
+	for (int i = 0; i < column; i++)
+	{
+		if (CheckThisRow(beginIndex, playerX, 3))
+		{
+			isWin = true;
+			break;
+		}
+		else if (CheckThisRow(beginIndex, playerO, 3))
+		{
+			isWin = true;
+			break;
+		}
+		beginIndex += 1;
+	}
+	return isWin;
+}
+
+bool CheckThisRow(int beginIndex, int player, int toAdd)
+{
+	if (usedSpace[beginIndex] == player && usedSpace[beginIndex + toAdd] == player && usedSpace[beginIndex + (toAdd * 2)] == player)
+	{
+		return true;
+	}
+	return false;
+}
+
+bool CheckCurvedLineWin()
+{
+	bool isWin = false;
+	int line = 2;
+	int secuondLine = 2; //box that begin in indix 2
+	int firstLine = 0; //box that begin at index 0
+	for (int i = 0; i < line; i++)
+	{
+
+		if (CheckThisRow(firstLine, playerX, 4))
+		{
+			isWin = true;
+			break;
+		}
+		else if (CheckThisRow(firstLine, playerO, 4))
+		{
+			isWin = true;
+			break;
+		}
+	}
+	for (int i = 0; i < line; i++)
+	{
+		if (CheckThisRow(secuondLine, playerX, 2))
+		{
+			isWin = true;
+			break;
+		}
+		else if (CheckThisRow(secuondLine, playerO, 2))
+		{
+			isWin = true;
+			break;
+		}
+	}
+	return isWin;
+}
+
+void ShowMenu()
+{
+	system("cls");
+	std::cout << "================================" << std::endl;
+	std::cout << "Welcome to the Tic Tac Toe Game!" << std::endl;
+	std::cout << "================================" << std::endl;
+	std::cout << "1. Play New Game" << std::endl;
+	std::cout << "0. Exit" << std::endl;
 }
 
 
